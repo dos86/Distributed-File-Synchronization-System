f6fc76f74cfdb8862fc483e3e296db5730590390
dos86 <157111350+dos86@users.noreply.github.com>
2025-05-03 16:12:26 +0200

    Update Encryption.cpp

--- src/Encryption.cpp
+++ src/Encryption.cpp
@@ -3,11 +3,23 @@
 #include <cstring>
 
 std::string Encryption::encrypt(const std::string& data, const std::string& key) {
-    // Implement AES-256 encryption
-    return "";
+    AES_KEY aesKey;
+    unsigned char encryptedData[1024];
+    memset(encryptedData, 0, sizeof(encryptedData));
+
+    AES_set_encrypt_key((unsigned char*)key.c_str(), 256, &aesKey);
+    AES_encrypt((unsigned char*)data.c_str(), encryptedData, &aesKey);
+
+    return std::string((char*)encryptedData);
 }
 
 std::string Encryption::decrypt(const std::string& encryptedData, const std::string& key) {
-    // Implement AES-256 decryption
-    return "";
+    AES_KEY aesKey;
+    unsigned char decryptedData[1024];
+    memset(decryptedData, 0, sizeof(decryptedData));
+
+    AES_set_decrypt_key((unsigned char*)key.c_str(), 256, &aesKey);
+    AES_decrypt((unsigned char*)encryptedData.c_str(), decryptedData, &aesKey);
+
+    return std::string((char*)decryptedData);
 }
